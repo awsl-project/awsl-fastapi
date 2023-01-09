7df7a06beeee15e76e2102c1dd480254ac90e6d0
Dream Hunter <dreamhunter2333@gmail.com>
2023-01-09 10:55:25 +0800

    Update moyu_config.py

--- router/moyu_config.py
+++ router/moyu_config.py
@@ -35,40 +35,45 @@ MO_YU_TEMPLATE_DAY_N = """
  距离【周六】还有 {day_to_weekend} 天
 """
 
-QINGMING_DATE = datetime(2022, 4, 3, tzinfo=TZ)
+SPRINGFEST_DATE = datetime(2023, 1, 22, tzinfo=TZ)
+SPRINGFEST = """
+ 距离【春节】还有 {day} 天 {hour} 小时。春节：1月21日至27日放假调休，共7天。
+"""
+
+QINGMING_DATE = datetime(2023, 4, 5, tzinfo=TZ)
 QINGMING = """
- 距离【清明】还有 {day} 天 {hour} 小时。清明节: 4 月 3 日 至 5 日放假调休, 共 3 天。4 月 2 日(星期六)上班。
+ 距离【清明】还有 {day} 天 {hour} 小时。清明节：4月5日放假，共 1 天。
 """
 
-WUYI_DATE = datetime(2022, 5, 1, tzinfo=TZ)
+WUYI_DATE = datetime(2023, 5, 1, tzinfo=TZ)
 WUYI = """
- 距离【五一】还有 {day} 天 {hour} 小时。劳动节: 4 月 30 日 至 5 月 4 日放假调休, 共 5 天。4 月 24 日(星期日)、5 月 7 日(星期六)上班。
+ 距离【五一】还有 {day} 天 {hour} 小时。劳动节: 劳动节：4 月 29 日 至 5 月 3 日放假调休，共5天。4月23日（星期日）、5月6日（星期六）上班。
 """
 
-DUANWU_DATE = datetime(2022, 6, 3, tzinfo=TZ)
+DUANWU_DATE = datetime(2023, 6, 22, tzinfo=TZ)
 DUANWU = """
- 距离【端午】还有 {day} 天 {hour} 小时。端午节: 6 月 3 日 至 5 日放假, 共 3 天。
+ 距离【端午】还有 {day} 天 {hour} 小时。端午节：6 月 22 日 至 24 日放假调休，共 3 天。6月25日（星期日）上班。
 """
 
-ZHONGQIU_DATE = datetime(2022, 9, 10, tzinfo=TZ)
+ZHONGQIU_DATE = datetime(2022, 9, 29, tzinfo=TZ)
 ZHONGQIU = """
- 距离【中秋】还有 {day} 天 {hour} 小时。中秋节: 9 月 10 日 至 12 日放假, 共 3 天。
+ 距离【中秋+国庆】还有 {day} 天 {hour} 小时。中秋节、国庆节：9 月 29 日 至 10 月 6 日放假调休，共8天。10月7日（星期六）、10月8日（星期日）上班。
 """
 
-GUOQING_DATE = datetime(2022, 10, 1, tzinfo=TZ)
-GUOQING = """
- 距离【国庆】还有 {day} 天 {hour} 小时。国庆节: 10 月1 日 至 7 日放假调休, 共 7 天。
-"""
+# GUOQING_DATE = datetime(2022, 10, 1, tzinfo=TZ)
+# GUOQING = """
+#  距离【国庆】还有 {day} 天 {hour} 小时。国庆节: 10 月1 日 至 7 日放假调休, 共 7 天。
+# """
 
-NEWYEAR_DATE = datetime(2022, 1, 1, tzinfo=TZ)
+NEWYEAR_DATE = datetime(2023, 1, 1, tzinfo=TZ)
 NEWYEAR = """
  距离【元旦】还有 {day} 天 {hour} 小时。
 """
 
-SPRINGFEST_DATE = datetime(2023, 1, 22, tzinfo=TZ)
-SPRINGFEST = """
- 距离【春节】还有 {day} 天 {hour} 小时。
-"""
+# SPRINGFEST_DATE = datetime(2023, 1, 22, tzinfo=TZ)
+# SPRINGFEST = """
+#  距离【春节】还有 {day} 天 {hour} 小时。春节：1月21日至27日放假调休，共7天。
+# """
 
 FEST_MAP: Dict[datetime, str] = {
     QINGMING_DATE: QINGMING,
